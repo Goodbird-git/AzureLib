56a56421b5e7ace840006c9b88b4f48975dc9686
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-05-14 14:28:51 -0400

    Update GeoVertex.java

--- Forge/src/main/java/mod/azure/azurelib/cache/object/GeoVertex.java
+++ Forge/src/main/java/mod/azure/azurelib/cache/object/GeoVertex.java
@@ -2,17 +2,17 @@ package mod.azure.azurelib.cache.object;
 
 import net.minecraft.util.math.vector.Vector3f;
 
-/**
- * Vertex data holder
- * @param position The position of the vertex
- * @param texU The texture U coordinate
- * @param texV The texture V coordinate
- */
 public class GeoVertex {
 	protected final Vector3f position;
 	protected final float texU;
 	protected final float texV;
-	
+
+	/**
+	 * Vertex data holder
+	 * @param position The position of the vertex
+	 * @param texU The texture U coordinate
+	 * @param texV The texture V coordinate
+	 */
 	public GeoVertex(final Vector3f position, final float texU, final float texV) {
 		this.position = position;
 		this.texU = texU;
