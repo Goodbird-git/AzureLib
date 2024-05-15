7dbdf86e874d77808cdb4234947e6d4b9ba3e3e1
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-05-15 00:59:41 -0400

    Only register keybinds if my mods are installed

--- fabric/src/main/java/mod/azure/azurelib/fabric/ClientListener.java
+++ fabric/src/main/java/mod/azure/azurelib/fabric/ClientListener.java
@@ -1,38 +1,48 @@
 package mod.azure.azurelib.fabric;
 
 import com.mojang.blaze3d.platform.InputConstants;
+import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
 import mod.azure.azurelib.common.api.client.helper.ClientUtils;
 import mod.azure.azurelib.common.platform.services.AzureLibNetwork;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.KeyMapping;
 import org.lwjgl.glfw.GLFW;
 
+import java.util.Set;
+
 public final class ClientListener implements ClientModInitializer {
+
+    private static final Set<String> MYGUNMODS = ObjectOpenHashSet.of(
+            "doom", "hwg", "arachnids", "aftershock", "mchalo"
+    );
 
     @Override
     public void onInitializeClient() {
-        ClientUtils.RELOAD = new KeyMapping(
-                "key.azurelib.reload",
-                InputConstants.Type.KEYSYM,
-                GLFW.GLFW_KEY_R,
-                "category.azurelib.binds"
-        );
-        KeyBindingHelper.registerKeyBinding(ClientUtils.RELOAD);
-        ClientUtils.SCOPE = new KeyMapping(
-                "key.azurelib.scope",
-                InputConstants.Type.KEYSYM,
-                GLFW.GLFW_KEY_LEFT_ALT,
-                "category.azurelib.binds"
-        );
-        KeyBindingHelper.registerKeyBinding(ClientUtils.SCOPE);
-        ClientUtils.FIRE_WEAPON = new KeyMapping(
-                "key.azurelib.fire",
-                InputConstants.Type.KEYSYM,
-                GLFW.GLFW_KEY_UNKNOWN,
-                "category.azurelib.binds"
-        );
-        KeyBindingHelper.registerKeyBinding(ClientUtils.FIRE_WEAPON);
+        if (MYGUNMODS.stream().anyMatch(FabricLoader.getInstance()::isModLoaded)) {
+            ClientUtils.RELOAD = new KeyMapping(
+                    "key.azurelib.reload",
+                    InputConstants.Type.KEYSYM,
+                    GLFW.GLFW_KEY_R,
+                    "category.azurelib.binds"
+            );
+            KeyBindingHelper.registerKeyBinding(ClientUtils.RELOAD);
+            ClientUtils.SCOPE = new KeyMapping(
+                    "key.azurelib.scope",
+                    InputConstants.Type.KEYSYM,
+                    GLFW.GLFW_KEY_LEFT_ALT,
+                    "category.azurelib.binds"
+            );
+            KeyBindingHelper.registerKeyBinding(ClientUtils.SCOPE);
+            ClientUtils.FIRE_WEAPON = new KeyMapping(
+                    "key.azurelib.fire",
+                    InputConstants.Type.KEYSYM,
+                    GLFW.GLFW_KEY_UNKNOWN,
+                    "category.azurelib.binds"
+            );
+            KeyBindingHelper.registerKeyBinding(ClientUtils.FIRE_WEAPON);
+        }
         AzureLibNetwork.init();
     }
 }
