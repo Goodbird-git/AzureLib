2b389c44574e28b790ff21ef25c2b16167d83f6e
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-05-14 20:42:53 -0400

    Update ClientListener.java

--- Forge/src/main/java/mod/azure/azurelib/ClientListener.java
+++ Forge/src/main/java/mod/azure/azurelib/ClientListener.java
@@ -2,7 +2,6 @@ package mod.azure.azurelib;
 
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import org.lwjgl.glfw.GLFW;
-import mod.azure.azurelib.testing.TestingEntityRenderer;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -12,12 +11,6 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 @EventBusSubscriber(modid = AzureLib.MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ClientListener {
-
-	@SubscribeEvent
-	public static void registerRenderers(final FMLClientSetupEvent event) {
-		RenderingRegistry.registerEntityRenderingHandler(AzureLibMod.GEO_EXAMPLE_ENTITY.get(),
-				TestingEntityRenderer::new);
-	}
 
 	@SubscribeEvent
 	public static void registerKeys(final FMLClientSetupEvent  event) {
