2ff99e03c8859cd0270b61e327a4555edbda4c84
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-05-28 16:55:46 -0400

    Only enable my keybinds if my mods are installed

--- neo/src/main/java/mod/azure/azurelib/neoforge/ClientModListener.java
+++ neo/src/main/java/mod/azure/azurelib/neoforge/ClientModListener.java
@@ -1,6 +1,7 @@
 package mod.azure.azurelib.neoforge;
 
 import com.mojang.blaze3d.platform.InputConstants;
+import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
 import mod.azure.azurelib.common.api.client.helper.ClientUtils;
 import mod.azure.azurelib.common.internal.client.AzureLibClient;
 import mod.azure.azurelib.common.internal.common.AzureLib;
@@ -20,21 +21,29 @@ import org.lwjgl.glfw.GLFW;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 
 @EventBusSubscriber(modid = AzureLib.MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ClientModListener {
 
+    private static final Set<String> MYGUNMODS = ObjectOpenHashSet.of(
+            "doom", "hwg", "arachnids", "aftershock", "mchalo"
+    );
+
     @SubscribeEvent
     public static void registerKeys(final RegisterKeyMappingsEvent event) {
-        ClientUtils.RELOAD = new KeyMapping("key.azurelib.reload", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_R,
-                "category.azurelib.binds");
-        event.register(ClientUtils.RELOAD);
-        ClientUtils.SCOPE = new KeyMapping("key.azurelib.scope", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_LEFT_ALT,
-                "category.azurelib.binds");
-        event.register(ClientUtils.SCOPE);
-        ClientUtils.FIRE_WEAPON = new KeyMapping("key.azurelib.fire", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN,
-                "category.azurelib.binds");
-        event.register(ClientUtils.FIRE_WEAPON);
+        if (ModList.get().isLoaded(MYGUNMODS.stream().toString())) {
+            ClientUtils.RELOAD = new KeyMapping("key.azurelib.reload", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_R,
+                    "category.azurelib.binds");
+            event.register(ClientUtils.RELOAD);
+            ClientUtils.SCOPE = new KeyMapping("key.azurelib.scope", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_LEFT_ALT,
+                    "category.azurelib.binds");
+            event.register(ClientUtils.SCOPE);
+            ClientUtils.FIRE_WEAPON = new KeyMapping("key.azurelib.fire", InputConstants.Type.KEYSYM,
+                    GLFW.GLFW_KEY_UNKNOWN,
+                    "category.azurelib.binds");
+            event.register(ClientUtils.FIRE_WEAPON);
+        }
     }
 
     @SubscribeEvent
