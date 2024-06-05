4298ee0ba7540c421152196a9dd16cd3bb8a6ddf
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-06-05 17:16:15 -0400

    Remove unneeded

--- common/src/main/java/mod/azure/azurelib/common/platform/services/AzureLibNetwork.java
+++ common/src/main/java/mod/azure/azurelib/common/platform/services/AzureLibNetwork.java
@@ -13,6 +13,7 @@ import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerEntity;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
@@ -86,8 +87,6 @@ public interface AzureLibNetwork {
                 AzureLib.LOGGER.debug("Registered SyncedAnimatable for {}", animatable.getClass());
         }
     }
-
-    Packet<?> createPacket(Entity entity);
 
     void sendToTrackingEntityAndSelf(AbstractPacket packet, Entity entityToTrack);
 
--- fabric/src/main/java/mod/azure/azurelib/fabric/platform/FabricAzureLibNetwork.java
+++ fabric/src/main/java/mod/azure/azurelib/fabric/platform/FabricAzureLibNetwork.java
@@ -14,6 +14,7 @@ import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
+import net.minecraft.server.level.ServerEntity;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
@@ -33,11 +34,6 @@ public class FabricAzureLibNetwork implements AzureLibNetwork {
             PayloadTypeRegistry.playC2S().register(payloadType, (StreamCodec<FriendlyByteBuf, P>) codec);
             ServerPlayNetworking.registerGlobalReceiver(payloadType, (packet, context) -> packet.handle());
         }
-    }
-
-    @Override
-    public Packet<?> createPacket(Entity entity) {
-        return entity.getAddEntityPacket();
     }
 
     public FriendlyByteBuf createFriendlyByteBuf() {
