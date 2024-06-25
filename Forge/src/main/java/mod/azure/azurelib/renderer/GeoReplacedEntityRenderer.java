211d71232bbcbf0e367f621b2cf22b6446225976
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-06-25 03:05:13 -0400

    Add support for scale attribute/missing shaking for GeoReplaced

--- Forge/src/main/java/mod/azure/azurelib/renderer/GeoReplacedEntityRenderer.java
+++ Forge/src/main/java/mod/azure/azurelib/renderer/GeoReplacedEntityRenderer.java
@@ -28,6 +28,7 @@ import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
@@ -201,11 +202,13 @@ public class GeoReplacedEntityRenderer<E extends Entity, T extends GeoAnimatable
 			}
 		}
 
+		float nativeScale = livingEntity != null ? livingEntity.getScale() : 1;
 		float ageInTicks = this.currentEntity.tickCount + partialTick;
 		float limbSwingAmount = 0;
 		float limbSwing = 0;
 
-		applyRotations(animatable, poseStack, ageInTicks, lerpBodyRot, partialTick);
+		poseStack.scale(nativeScale, nativeScale, nativeScale);
+		applyRotations(animatable, poseStack, ageInTicks, lerpBodyRot, partialTick, nativeScale);
 
 		if (!shouldSit && this.currentEntity.isAlive() && livingEntity != null) {
 			limbSwingAmount = Mth.lerp(partialTick, livingEntity.animationSpeedOld, livingEntity.animationSpeed);
@@ -319,45 +322,42 @@ public class GeoReplacedEntityRenderer<E extends Entity, T extends GeoAnimatable
 	}
 
 	/**
-	 * Applies rotation transformations to the renderer prior to render time to account for various entity states
+	 * Applies rotation transformations to the renderer prior to render time to account for various entity states, default scale of 1
+	 */
+	protected void applyRotations(T animatable, PoseStack poseStack, float ageInTicks, float rotationYaw, float partialTick) {
+		applyRotations(animatable, poseStack, ageInTicks, rotationYaw, partialTick, 1);
+	}
+
+	/**
+	 * Applies rotation transformations to the renderer prior to render time to account for various entity states, scalable
 	 */
 	protected void applyRotations(T animatable, PoseStack poseStack, float ageInTicks, float rotationYaw,
-								  float partialTick) {
-		Pose pose = this.currentEntity.getPose();
-		LivingEntity livingEntity = this.currentEntity instanceof LivingEntity entity ? entity : null;
+								  float partialTick, float nativeScale) {
+		if (isShaking(animatable))
+			rotationYaw += (float)(Math.cos(this.currentEntity.tickCount * 3.25d) * Math.PI * 0.4d);
 
-		if (pose != Pose.SLEEPING)
+		if (!this.currentEntity.hasPose(Pose.SLEEPING))
 			poseStack.mulPose(Vector3f.YP.rotationDegrees(180f - rotationYaw));
 
-		if (livingEntity != null && livingEntity.deathTime > 0) {
-			float deathRotation = (livingEntity.deathTime + partialTick - 1f) / 20f * 1.6f;
+		if (this.currentEntity instanceof LivingEntity livingEntity) {
+			if (livingEntity.deathTime > 0) {
+				float deathRotation = (livingEntity.deathTime + partialTick - 1f) / 20f * 1.6f;
 
-			poseStack.mulPose(Vector3f.ZP.rotationDegrees(Math.min(Mth.sqrt(deathRotation), 1) * getDeathMaxRotation(animatable)));
-		}
-		else if (livingEntity != null && livingEntity.isAutoSpinAttack()) {
-			poseStack.mulPose(Vector3f.XP.rotationDegrees(-90f - livingEntity.getXRot()));
-			poseStack.mulPose(Vector3f.YP.rotationDegrees((livingEntity.tickCount + partialTick) * -75f));
-		}
-		else if (livingEntity != null && pose == Pose.SLEEPING) {
-			Direction bedOrientation = livingEntity.getBedOrientation();
-
-			poseStack.mulPose(Vector3f.YP.rotationDegrees(bedOrientation != null ? RenderUtils.getDirectionAngle(bedOrientation) : rotationYaw));
-			poseStack.mulPose(Vector3f.ZP.rotationDegrees(getDeathMaxRotation(animatable)));
-			poseStack.mulPose(Vector3f.YP.rotationDegrees(270f));
-		}
-		else if (this.currentEntity.hasCustomName() || this.currentEntity instanceof Player) {
-			String name = this.currentEntity.getName().getString();
-
-			if (this.currentEntity instanceof Player player) {
-				if (!player.isModelPartShown(PlayerModelPart.CAPE))
-					return;
+				poseStack.mulPose(Vector3f.ZP.rotationDegrees(Math.min(Mth.sqrt(deathRotation), 1) * getDeathMaxRotation(animatable)));
 			}
-			else {
-				name = ChatFormatting.stripFormatting(name);
+			else if (livingEntity.isAutoSpinAttack()) {
+				poseStack.mulPose(Vector3f.XP.rotationDegrees(-90f - livingEntity.getXRot()));
+				poseStack.mulPose(Vector3f.YP.rotationDegrees((livingEntity.tickCount + partialTick) * -75f));
 			}
+			else if (livingEntity.hasPose(Pose.SLEEPING)) {
+				Direction bedOrientation = livingEntity.getBedOrientation();
 
-			if (name != null && (name.equals("Dinnerbone") || name.equalsIgnoreCase("Grumm"))) {
-				poseStack.translate(0, this.currentEntity.getBbHeight() + 0.1f, 0);
+				poseStack.mulPose(Vector3f.YP.rotationDegrees(bedOrientation != null ? RenderUtils.getDirectionAngle(bedOrientation) : rotationYaw));
+				poseStack.mulPose(Vector3f.ZP.rotationDegrees(getDeathMaxRotation(animatable)));
+				poseStack.mulPose(Vector3f.YP.rotationDegrees(270f));
+			}
+			else if (LivingEntityRenderer.isEntityUpsideDown(livingEntity)) {
+				poseStack.translate(0, (livingEntity.getBbHeight() + 0.1f) / nativeScale, 0);
 				poseStack.mulPose(Vector3f.ZP.rotationDegrees(180f));
 			}
 		}
@@ -499,6 +499,10 @@ public class GeoReplacedEntityRenderer<E extends Entity, T extends GeoAnimatable
 
 		buffer.vertex(positionMatrix, x - xOffset, y + yOffset, z + zOffset).color(red, green, blue, 1).uv2(packedLight).endVertex();
 		buffer.vertex(positionMatrix, x + xOffset, y + width - yOffset, z - zOffset).color(red, green, blue, 1).uv2(packedLight).endVertex();
+	}
+
+	public boolean isShaking(T entity) {
+		return this.currentEntity.isFullyFrozen();
 	}
 	
 	/**
