24924ce7a19fd1af2de4b33c7a1f810a9541a91f
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-07-03 02:46:02 -0400

    Remove Fabric build

--- Fabric/src/main/java/mod/azure/azurelib/core/molang/MolangException.java
+++
@@ -1,9 +0,0 @@
-package mod.azure.azurelib.core.molang;
-
-public class MolangException extends Exception {
-	private static final long serialVersionUID = 1470247726869768015L;
-
-	public MolangException(String message) {
-		super(message);
-	}
-}
