ff0f07ad148bb520a4e53ac4b630c3c7333733bc
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-07-03 01:03:11 -0400

    Adds headers to Configuration forked classes
    I figured I'd do this as well since I'm here and I have this automated by a Python script.

--- common/src/main/java/mod/azure/azurelib/client/WidgetAdder.java
+++ common/src/main/java/mod/azure/azurelib/client/WidgetAdder.java
@@ -1,3 +1,9 @@
+/**
+ * This class is a fork of the matching class found in the Configuration repository.
+ * Original source: https://github.com/Toma1O6/Configuration
+ * Copyright © 2024 Toma1O6.
+ * Licensed under the MIT License.
+ */
 package mod.azure.azurelib.client;
 
 import net.minecraft.client.gui.components.AbstractWidget;
