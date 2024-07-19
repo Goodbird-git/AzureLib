bbe2f49bc4c33c35b49dce4dc29180164cf8e6c5
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-07-19 14:16:25 -0400

    Closes service in a safer way.

--- common/src/main/java/mod/azure/azurelib/config/io/FileWatchManager.java
+++ common/src/main/java/mod/azure/azurelib/config/io/FileWatchManager.java
@@ -30,6 +30,7 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 import mod.azure.azurelib.AzureLib;
+import mod.azure.azurelib.AzureLibException;
 import org.apache.logging.log4j.Marker;
 import org.apache.logging.log4j.MarkerManager;
 import org.jetbrains.annotations.Nullable;
@@ -64,7 +65,12 @@ public final class FileWatchManager {
     }
     
     public void stopService() {
-    	this.executorService.shutdown();
+        try {
+            this.executorService.shutdown();
+            this.service.close();
+        } catch (IOException e) {
+            throw new AzureLibException("Error while stopping FileWatch service", e);
+        }
     }
 
     public void startService() {
