80606cb65b09c5507bdc47611b1a3cc0cd014f96
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-09-06 15:29:38 -0400

    Fixes crash when loaded with Born in Chaos.

--- Forge/src/main/java/mod/azure/azurelib/cache/AzureLibCache.java
+++ Forge/src/main/java/mod/azure/azurelib/cache/AzureLibCache.java
@@ -1,15 +1,5 @@
 package mod.azure.azurelib.cache;
 
-import java.util.Collections;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
-import java.util.function.BiConsumer;
-import java.util.function.Function;
-
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
 import mod.azure.azurelib.AzureLib;
@@ -30,85 +20,103 @@ import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.util.profiling.ProfilerFiller;
 import net.minecraftforge.fml.ModLoader;
 
+import java.util.Collections;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+
 /**
  * Cache class for holding loaded {@link mod.azure.azurelib.core.animation.Animation Animations} and {@link CoreGeoModel Models}
  */
 public final class AzureLibCache {
-	private static final Set<String> EXCLUDED_NAMESPACES = ObjectOpenHashSet.of("geckolib3", "animatedmobsmod", "moreplayermodels", "customnpcs", "gunsrpg", "mimic", "celestisynth", "the_flesh_that_hates", "enemyexpansion", "mutationcraft", "dungeons_mobs", "fear_the_dark");
+    private static final Set<String> EXCLUDED_NAMESPACES = ObjectOpenHashSet.of("geckolib3", "animatedmobsmod",
+            "moreplayermodels", "customnpcs", "gunsrpg", "mimic", "celestisynth", "the_flesh_that_hates",
+            "enemyexpansion", "mutationcraft", "dungeons_mobs", "fear_the_dark", "born_in_chaos_v1");
 
-	private static Map<ResourceLocation, BakedAnimations> ANIMATIONS = Collections.emptyMap();
-	private static Map<ResourceLocation, BakedGeoModel> MODELS = Collections.emptyMap();
+    private static Map<ResourceLocation, BakedAnimations> ANIMATIONS = Collections.emptyMap();
+    private static Map<ResourceLocation, BakedGeoModel> MODELS = Collections.emptyMap();
 
-	public static Map<ResourceLocation, BakedAnimations> getBakedAnimations() {
-		if (!AzureLib.hasInitialized)
-			throw new RuntimeException("AzureLib was never initialized! Please read the documentation!");
+    public static Map<ResourceLocation, BakedAnimations> getBakedAnimations() {
+        if (!AzureLib.hasInitialized)
+            throw new RuntimeException("AzureLib was never initialized! Please read the documentation!");
 
-		return ANIMATIONS;
-	}
+        return ANIMATIONS;
+    }
 
-	public static Map<ResourceLocation, BakedGeoModel> getBakedModels() {
-		if (!AzureLib.hasInitialized)
-			throw new RuntimeException("AzureLib was never initialized! Please read the documentation!");
+    public static Map<ResourceLocation, BakedGeoModel> getBakedModels() {
+        if (!AzureLib.hasInitialized)
+            throw new RuntimeException("AzureLib was never initialized! Please read the documentation!");
 
-		return MODELS;
-	}
+        return MODELS;
+    }
 
-	public static void registerReloadListener() {
-		Minecraft mc = Minecraft.getInstance();
+    public static void registerReloadListener() {
+        Minecraft mc = Minecraft.getInstance();
 
-		if (mc == null) {
-			if (!ModLoader.isDataGenRunning())
-				AzureLib.LOGGER.warn("Minecraft.getInstance() was null, could not register reload listeners");
+        if (mc == null) {
+            if (!ModLoader.isDataGenRunning())
+                AzureLib.LOGGER.warn("Minecraft.getInstance() was null, could not register reload listeners");
 
-			return;
-		}
+            return;
+        }
 
-		if (!(mc.getResourceManager() instanceof ReloadableResourceManager resourceManager))
-			throw new RuntimeException("AzureLib was initialized too early!");
+        if (!(mc.getResourceManager() instanceof ReloadableResourceManager resourceManager))
+            throw new RuntimeException("AzureLib was initialized too early!");
 
-		resourceManager.registerReloadListener(AzureLibCache::reload);
-	}
+        resourceManager.registerReloadListener(AzureLibCache::reload);
+    }
 
-	private static CompletableFuture<Void> reload(PreparationBarrier stage, ResourceManager resourceManager, ProfilerFiller preparationsProfiler, ProfilerFiller reloadProfiler, Executor backgroundExecutor, Executor gameExecutor) {
-		Map<ResourceLocation, BakedAnimations> animations = new Object2ObjectOpenHashMap<>();
-		Map<ResourceLocation, BakedGeoModel> models = new Object2ObjectOpenHashMap<>();
+    private static CompletableFuture<Void> reload(PreparationBarrier stage, ResourceManager resourceManager, ProfilerFiller preparationsProfiler, ProfilerFiller reloadProfiler, Executor backgroundExecutor, Executor gameExecutor) {
+        Map<ResourceLocation, BakedAnimations> animations = new Object2ObjectOpenHashMap<>();
+        Map<ResourceLocation, BakedGeoModel> models = new Object2ObjectOpenHashMap<>();
 
-		return CompletableFuture.allOf(loadAnimations(backgroundExecutor, resourceManager, animations::put), loadModels(backgroundExecutor, resourceManager, models::put)).thenCompose(stage::wait).thenAcceptAsync(empty -> {
-			AzureLibCache.ANIMATIONS = animations;
-			AzureLibCache.MODELS = models;
-		}, gameExecutor);
-	}
+        return CompletableFuture.allOf(loadAnimations(backgroundExecutor, resourceManager, animations::put),
+                loadModels(backgroundExecutor, resourceManager, models::put)).thenCompose(stage::wait).thenAcceptAsync(
+                empty -> {
+                    AzureLibCache.ANIMATIONS = animations;
+                    AzureLibCache.MODELS = models;
+                }, gameExecutor);
+    }
 
-	private static CompletableFuture<Void> loadAnimations(Executor backgroundExecutor, ResourceManager resourceManager, BiConsumer<ResourceLocation, BakedAnimations> elementConsumer) {
-		return loadResources(backgroundExecutor, resourceManager, "animations", resource -> FileLoader.loadAnimationsFile(resource, resourceManager), elementConsumer);
-	}
+    private static CompletableFuture<Void> loadAnimations(Executor backgroundExecutor, ResourceManager resourceManager, BiConsumer<ResourceLocation, BakedAnimations> elementConsumer) {
+        return loadResources(backgroundExecutor, resourceManager, "animations",
+                resource -> FileLoader.loadAnimationsFile(resource, resourceManager), elementConsumer);
+    }
 
-	private static CompletableFuture<Void> loadModels(Executor backgroundExecutor, ResourceManager resourceManager, BiConsumer<ResourceLocation, BakedGeoModel> elementConsumer) {
-		return loadResources(backgroundExecutor, resourceManager, "geo", resource -> {
-			Model model = FileLoader.loadModelFile(resource, resourceManager);
+    private static CompletableFuture<Void> loadModels(Executor backgroundExecutor, ResourceManager resourceManager, BiConsumer<ResourceLocation, BakedGeoModel> elementConsumer) {
+        return loadResources(backgroundExecutor, resourceManager, "geo", resource -> {
+            Model model = FileLoader.loadModelFile(resource, resourceManager);
 
-			if (model.formatVersion() != FormatVersion.V_1_12_0)
-				throw new AzureLibException(resource, "Unsupported geometry json version. Supported versions: 1.12.0");
+            if (model.formatVersion() != FormatVersion.V_1_12_0)
+                throw new AzureLibException(resource, "Unsupported geometry json version. Supported versions: 1.12.0");
 
-			return BakedModelFactory.getForNamespace(resource.getNamespace()).constructGeoModel(GeometryTree.fromModel(model));
-		}, elementConsumer);
-	}
+            return BakedModelFactory.getForNamespace(resource.getNamespace()).constructGeoModel(
+                    GeometryTree.fromModel(model));
+        }, elementConsumer);
+    }
 
-	private static <T> CompletableFuture<Void> loadResources(Executor executor, ResourceManager resourceManager, String type, Function<ResourceLocation, T> loader, BiConsumer<ResourceLocation, T> map) {
-		return CompletableFuture.supplyAsync(() -> resourceManager.listResources(type, fileName -> fileName.toString().endsWith(".json")), executor).thenApplyAsync(resources -> {
-			Map<ResourceLocation, CompletableFuture<T>> tasks = new Object2ObjectOpenHashMap<>();
+    private static <T> CompletableFuture<Void> loadResources(Executor executor, ResourceManager resourceManager, String type, Function<ResourceLocation, T> loader, BiConsumer<ResourceLocation, T> map) {
+        return CompletableFuture.supplyAsync(
+                () -> resourceManager.listResources(type, fileName -> fileName.toString().endsWith(".json")),
+                executor).thenApplyAsync(resources -> {
+            Map<ResourceLocation, CompletableFuture<T>> tasks = new Object2ObjectOpenHashMap<>();
 
-			for (ResourceLocation resource : resources.keySet()) {
-				tasks.put(resource, CompletableFuture.supplyAsync(() -> loader.apply(resource), executor));
-			}
+            for (ResourceLocation resource : resources.keySet()) {
+                tasks.put(resource, CompletableFuture.supplyAsync(() -> loader.apply(resource), executor));
+            }
 
-			return tasks;
-		}, executor).thenAcceptAsync(tasks -> {
-			for (Entry<ResourceLocation, CompletableFuture<T>> entry : tasks.entrySet()) {
-				// Skip known namespaces that use an "animation" folder as well
-				if (!EXCLUDED_NAMESPACES.contains(entry.getKey().getNamespace().toLowerCase(Locale.ROOT)))
-					map.accept(entry.getKey(), entry.getValue().join());
-			}
-		}, executor);
-	}
+            return tasks;
+        }, executor).thenAcceptAsync(tasks -> {
+            for (Entry<ResourceLocation, CompletableFuture<T>> entry : tasks.entrySet()) {
+                // Skip known namespaces that use an "animation" folder as well
+                if (!EXCLUDED_NAMESPACES.contains(entry.getKey().getNamespace().toLowerCase(Locale.ROOT)))
+                    map.accept(entry.getKey(), entry.getValue().join());
+            }
+        }, executor);
+    }
 }
