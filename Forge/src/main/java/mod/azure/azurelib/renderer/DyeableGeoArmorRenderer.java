ec3cbb2a22b163c17a80b09d7f4e3f1a4dae1669
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-09-15 20:27:40 -0400

    Tweaked DyeableGeoArmorRender to match on all MC versions

--- Forge/src/main/java/mod/azure/azurelib/renderer/DyeableGeoArmorRenderer.java
+++ Forge/src/main/java/mod/azure/azurelib/renderer/DyeableGeoArmorRenderer.java
@@ -18,6 +18,7 @@ import mod.azure.azurelib.model.GeoModel;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.item.Item;
 
+import javax.annotation.Nullable;
 import java.util.Collection;
 import java.util.Set;
 
@@ -26,37 +27,80 @@ import java.util.Set;
  */
 public abstract class DyeableGeoArmorRenderer<T extends Item & GeoItem> extends GeoArmorRenderer<T> {
     protected final Set<GeoBone> dyeableBones = new ObjectArraySet<>();
+    protected BakedGeoModel lastModel = null;
 
     protected DyeableGeoArmorRenderer(GeoModel<T> model) {
         super(model);
     }
 
     @Override
-    public void preRender(MatrixStack poseStack, T animatable, BakedGeoModel model, IRenderTypeBuffer bufferSource, IVertexBuilder buffer, boolean isReRender, float partialTick, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
-        super.preRender(poseStack, animatable, model, bufferSource, buffer, isReRender, partialTick, packedLight,
-                packedOverlay, red, green, blue, alpha);
+    public void preRender(
+            MatrixStack poseStack,
+            T animatable,
+            BakedGeoModel model,
+            @Nullable IRenderTypeBuffer bufferSource,
+            @Nullable IVertexBuilder buffer,
+            boolean isReRender,
+            float partialTick,
+            int packedLight,
+            int packedOverlay,
+            float red,
+            float green,
+            float blue,
+            float alpha
+    ) {
+        super.preRender(
+                poseStack,
+                animatable,
+                model,
+                bufferSource,
+                buffer,
+                isReRender,
+                partialTick,
+                packedLight,
+                packedOverlay,
+                red,
+                green,
+                blue,
+                alpha
+        );
 
         if (!isReRender)
             checkBoneDyeCache(model);
     }
 
     @Override
-    public void renderCubesOfBone(MatrixStack poseStack, GeoBone bone, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+    public void renderCubesOfBone(
+            MatrixStack poseStack,
+            GeoBone bone,
+            IVertexBuilder buffer,
+            int packedLight,
+            int packedOverlay,
+            float red,
+            float green,
+            float blue,
+            float alpha
+    ) {
         if (this.dyeableBones.contains(bone)) {
             final Color color = getColorForBone(bone);
 
-            red *= color.getRed();
-            green *= color.getGreen();
-            blue *= color.getBlue();
-            alpha *= color.getAlpha();
+            red *= (float) color.getRed();
+            green *= (float) color.getGreen();
+            blue *= (float) color.getBlue();
+            alpha *= (float) color.getAlpha();
         }
-
-        super.renderCubesOfBone(poseStack, bone, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+        super.renderCubesOfBone(poseStack, bone, buffer, packedLight, packedOverlay, red,
+                green,
+                blue,
+                alpha);
     }
 
     /**
      * Whether the given GeoBone should be considered dyeable or not.
-     * <p>Note that values returned from here are cached for the last rendered {@link BakedGeoModel} and require a manual reset if you intend to change these results.</p>
+     * <p>
+     * Note that values returned from here are cached for the last rendered {@link BakedGeoModel} and require a manual
+     * reset if you intend to change these results.
+     * </p>
      *
      * @return whether the bone should be dyed or not
      */
@@ -64,13 +108,18 @@ public abstract class DyeableGeoArmorRenderer<T extends Item & GeoItem> extends 
 
     /**
      * What color the given GeoBone should be dyed as.
-     * <p>Only bones that were marked as 'dyeable' in {@link DyeableGeoArmorRenderer#isBoneDyeable(GeoBone)} are provided here</p>
+     * <p>
+     * Only bones that were marked as 'dyeable' in {@link DyeableGeoArmorRenderer#isBoneDyeable(GeoBone)} are provided
+     * here
+     * </p>
      */
     protected abstract Color getColorForBone(GeoBone bone);
 
     /**
      * Check whether the dye cache should be considered dirty and recomputed.
-     * <p>The less this forces re-computation, the better for performance</p>
+     * <p>
+     * The less this forces re-computation, the better for performance
+     * </p>
      */
     protected void checkBoneDyeCache(BakedGeoModel model) {
         if (model != this.lastModel) {
