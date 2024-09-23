f83eb47bc5346759d2fe7bf6992ae0121b39c780
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-09-23 01:10:10 -0400

    Use root locale when converting strings - PlanetTeamSpeak

--- Fabric/src/main/java/mod/azure/azurelib/client/widget/ColorWidget.java
+++ Fabric/src/main/java/mod/azure/azurelib/client/widget/ColorWidget.java
@@ -14,6 +14,7 @@ import net.minecraft.network.chat.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.IntSupplier;
@@ -65,7 +66,7 @@ public final class ColorWidget extends AbstractWidget {
         ColorSelectorDialog dialog = new ColorSelectorDialog(SELECT_COLOR, this.lastScreen, this.argb, this.colorSupplier);
         dialog.onConfirmed(screen -> {
             int color = dialog.getResultColor();
-            String colorText = this.colorPrefix + Integer.toHexString(color).toUpperCase();
+            String colorText = this.colorPrefix + Integer.toHexString(color).toUpperCase(Locale.ROOT);
             this.colorWidget.set(colorText);
             dialog.displayPreviousScreen(dialog);
         });
@@ -216,7 +217,7 @@ public final class ColorWidget extends AbstractWidget {
             ColorComponent(int bitOffset) {
                 this.bitOffset = bitOffset;
                 this.title = val -> {
-                    String name = this.name().toLowerCase();
+                    String name = this.name().toLowerCase(Locale.ROOT);
                     String translate = "text.azurelib.screen.color." + name;
                     int colorValue = (int) (val * 255);
                     return Component.translatable(translate, colorValue);
