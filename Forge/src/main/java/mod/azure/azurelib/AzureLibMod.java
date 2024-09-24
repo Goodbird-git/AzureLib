eac782b385800b208dc3a096319ea52a7e166308
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-09-24 12:54:50 -0400

    Port to Forge as well so 1.19.4 has parity with Fabric

--- Forge/src/main/java/mod/azure/azurelib/AzureLibMod.java
+++ Forge/src/main/java/mod/azure/azurelib/AzureLibMod.java
@@ -1,5 +1,11 @@
 package mod.azure.azurelib;
 
+import mod.azure.azurelib.config.Config;
+import mod.azure.azurelib.config.ConfigHolder;
+import mod.azure.azurelib.config.TestingConfig;
+import mod.azure.azurelib.config.format.ConfigFormats;
+import mod.azure.azurelib.config.format.IConfigFormatHandler;
+import mod.azure.azurelib.config.io.ConfigIO;
 import mod.azure.azurelib.entities.TickingLightBlock;
 import mod.azure.azurelib.entities.TickingLightEntity;
 import net.minecraft.world.level.block.Block;
@@ -7,6 +13,7 @@ import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -17,13 +24,20 @@ import net.minecraftforge.registries.RegistryObject;
 public final class AzureLibMod {
 
 	public static AzureLibMod instance;
+	public static TestingConfig config;
 
 	public AzureLibMod() {
 		instance = this;
 		final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 		AzureLib.initialize();
+		AzureLibMod.config = AzureLibMod.registerConfig(TestingConfig.class, ConfigFormats.json()).getConfigInstance();
+		modEventBus.addListener(this::init);
 		AzureBlocks.BLOCKS.register(modEventBus);
 		AzureEntities.TILE_TYPES.register(modEventBus);
+	}
+
+	private void init(FMLCommonSetupEvent event) {
+		ConfigIO.FILE_WATCH_MANAGER.startService();
 	}
 
 	public class AzureBlocks {
@@ -37,5 +51,35 @@ public final class AzureLibMod {
 		public static final DeferredRegister<BlockEntityType<?>> TILE_TYPES = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, AzureLib.MOD_ID);
 
 		public static final RegistryObject<BlockEntityType<TickingLightEntity>> TICKING_LIGHT_ENTITY = TILE_TYPES.register("lightblock", () -> BlockEntityType.Builder.of(TickingLightEntity::new, AzureBlocks.TICKING_LIGHT_BLOCK.get()).build(null));
+	}
+
+	/**
+	 * Registers your config class. Config will be immediately loaded upon calling.
+	 *
+	 * @param cfgClass      Your config class
+	 * @param formatFactory File format to be used by this config class. You can use values from {@link ConfigFormats} for example.
+	 * @param <CFG>         Config type
+	 * @return Config holder containing your config instance. You obtain it by calling {@link ConfigHolder#getConfigInstance()} method.
+	 */
+	public static <CFG> ConfigHolder<CFG> registerConfig(Class<CFG> cfgClass, IConfigFormatHandler formatFactory) {
+		Config cfg = cfgClass.getAnnotation(Config.class);
+		if (cfg == null) {
+			throw new IllegalArgumentException("Config class must be annotated with '@Config' annotation");
+		}
+		String id = cfg.id();
+		String filename = cfg.filename();
+		if (filename.isEmpty()) {
+			filename = id;
+		}
+		String group = cfg.group();
+		if (group.isEmpty()) {
+			group = id;
+		}
+		ConfigHolder<CFG> holder = new ConfigHolder<>(cfgClass, id, filename, group, formatFactory);
+		ConfigHolder.registerConfig(holder);
+		if (cfgClass.getAnnotation(Config.NoAutoSync.class) == null) {
+			ConfigIO.FILE_WATCH_MANAGER.addTrackedConfig(holder);
+		}
+		return holder;
 	}
 }
--- Forge/src/main/java/mod/azure/azurelib/ClientListener.java
+++ Forge/src/main/java/mod/azure/azurelib/ClientListener.java
@@ -1,5 +1,11 @@
 package mod.azure.azurelib;
 
+import mod.azure.azurelib.client.AzureLibClient;
+import mod.azure.azurelib.config.ConfigHolder;
+import net.minecraftforge.client.ConfigScreenHandler;
+import net.minecraftforge.fml.ModContainer;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import org.lwjgl.glfw.GLFW;
 
 import net.minecraft.client.KeyMapping;
@@ -8,6 +14,10 @@ import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 @EventBusSubscriber(modid = AzureLib.MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ClientListener {
 
@@ -15,5 +25,25 @@ public class ClientListener {
 	public static void registerKeys(final RegisterKeyMappingsEvent event) {
 		Keybindings.RELOAD = new KeyMapping("key.azurelib.reload", GLFW.GLFW_KEY_R, "category.azurelib.binds");
 		event.register(Keybindings.RELOAD);
+	}
+
+	@SubscribeEvent
+	public static void clientInit(final FMLClientSetupEvent event) {
+		Map<String, List<ConfigHolder<?>>> groups = ConfigHolder.getConfigGroupingByGroup();
+		ModList modList = ModList.get();
+		for (Map.Entry<String, List<ConfigHolder<?>>> entry : groups.entrySet()) {
+			String modId = entry.getKey();
+			Optional<? extends ModContainer> optional = modList.getModContainerById(modId);
+			optional.ifPresent(modContainer -> {
+				List<ConfigHolder<?>> list = entry.getValue();
+				modContainer.registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class,
+						() -> new ConfigScreenHandler.ConfigScreenFactory((minecraft, screen) -> {
+							if (list.size() == 1) {
+								return AzureLibClient.getConfigScreen(list.get(0).getConfigId(), screen);
+							}
+							return AzureLibClient.getConfigScreenByGroup(list, modId, screen);
+						}));
+			});
+		}
 	}
 }
