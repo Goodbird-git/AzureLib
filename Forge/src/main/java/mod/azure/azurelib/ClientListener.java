20c33d35bfb37fb0ebee2e7719c23ad65066f616
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-09-24 14:05:03 -0400

    Ports Config system to 1.18.2

--- Forge/src/main/java/mod/azure/azurelib/ClientListener.java
+++ Forge/src/main/java/mod/azure/azurelib/ClientListener.java
@@ -1,5 +1,10 @@
 package mod.azure.azurelib;
 
+import mod.azure.azurelib.client.AzureLibClient;
+import mod.azure.azurelib.config.ConfigHolder;
+import net.minecraftforge.client.ConfigGuiHandler;
+import net.minecraftforge.fml.ModContainer;
+import net.minecraftforge.fml.ModList;
 import org.lwjgl.glfw.GLFW;
 
 import net.minecraft.client.KeyMapping;
@@ -9,6 +14,10 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 @EventBusSubscriber(modid = AzureLib.MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ClientListener {
 
@@ -16,5 +25,24 @@ public class ClientListener {
 	public static void registerKeys(final FMLClientSetupEvent  event) {
 		Keybindings.RELOAD = new KeyMapping("key.azurelib.reload", GLFW.GLFW_KEY_R, "category.azurelib.binds");
 		ClientRegistry.registerKeyBinding(Keybindings.RELOAD);
+	}
+
+	@SubscribeEvent
+	public static void clientInit(final FMLClientSetupEvent event) {
+		Map<String, List<ConfigHolder<?>>> groups = ConfigHolder.getConfigGroupingByGroup();
+		ModList modList = ModList.get();
+		for (Map.Entry<String, List<ConfigHolder<?>>> entry : groups.entrySet()) {
+			String modId = entry.getKey();
+			Optional<? extends ModContainer> optional = modList.getModContainerById(modId);
+			optional.ifPresent(modContainer -> {
+				List<ConfigHolder<?>> list = entry.getValue();
+				modContainer.registerExtensionPoint(ConfigGuiHandler.ConfigGuiFactory.class, () -> new ConfigGuiHandler.ConfigGuiFactory((minecraft, screen) -> {
+					if (list.size() == 1) {
+						return AzureLibClient.getConfigScreen(list.get(0).getConfigId(), screen);
+					}
+					return AzureLibClient.getConfigScreenByGroup(list, modId, screen);
+				}));
+			});
+		}
 	}
 }
