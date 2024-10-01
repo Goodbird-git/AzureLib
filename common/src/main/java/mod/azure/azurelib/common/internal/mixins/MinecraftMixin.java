62fab8605ab60e24a460de4cad5a3a5ecf8f4598
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-10-01 00:15:18 -0400

    Final clean up

--- common/src/main/java/mod/azure/azurelib/common/internal/mixins/MinecraftMixin.java
+++ common/src/main/java/mod/azure/azurelib/common/internal/mixins/MinecraftMixin.java
@@ -4,12 +4,6 @@
  * Copyright © 2024 Toma1O6.
  * Licensed under the MIT License.
  */
-/**
- * This class is a fork of the matching class found in the Geckolib repository.
- * Original source: https://github.com/Toma1O6/Configuration
- * Copyright © 2024 Toma1O6.
- * Licensed under the MIT License.
- */
 package mod.azure.azurelib.common.internal.mixins;
 
 import com.mojang.blaze3d.platform.WindowEventHandler;
