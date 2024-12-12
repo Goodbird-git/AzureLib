03927e2fb94c174cf3e80d63d4dbcb60173c5b5f
Boston Vanseghi <bvanseg@gmail.com>
2024-12-12 15:55:38 -0500

    Implemented AzAnimationQueue.java, AzKeyFrameProcessor.java.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/mod/azure/azurelib/core2/animation/controller/AzAnimationController.java
+++ common/src/main/java/mod/azure/azurelib/core2/animation/controller/AzAnimationController.java
@@ -6,26 +6,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Collection;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
-import java.util.NoSuchElementException;
 import java.util.Objects;
-import java.util.Queue;
 import java.util.function.Function;
 import java.util.function.ToDoubleFunction;
 
 import mod.azure.azurelib.core.animatable.model.CoreGeoBone;
 import mod.azure.azurelib.core.animation.EasingType;
-import mod.azure.azurelib.core.keyframe.AnimationPoint;
 import mod.azure.azurelib.core.keyframe.BoneAnimationQueue;
-import mod.azure.azurelib.core.keyframe.Keyframe;
-import mod.azure.azurelib.core.keyframe.KeyframeLocation;
-import mod.azure.azurelib.core.math.Constant;
-import mod.azure.azurelib.core.math.IValue;
-import mod.azure.azurelib.core.molang.MolangParser;
-import mod.azure.azurelib.core.molang.MolangQueries;
-import mod.azure.azurelib.core.object.Axis;
 import mod.azure.azurelib.core.object.PlayState;
 import mod.azure.azurelib.core.state.BoneSnapshot;
 import mod.azure.azurelib.core2.animation.AzAnimationProcessor;
@@ -33,6 +21,7 @@ import mod.azure.azurelib.core2.animation.AzAnimationState;
 import mod.azure.azurelib.core2.animation.AzAnimator;
 import mod.azure.azurelib.core2.animation.controller.keyframe.AzKeyFrameCallbackManager;
 import mod.azure.azurelib.core2.animation.controller.keyframe.AzKeyFrameCallbacks;
+import mod.azure.azurelib.core2.animation.controller.keyframe.AzKeyFrameProcessor;
 import mod.azure.azurelib.core2.animation.primitive.AzAnimation;
 import mod.azure.azurelib.core2.animation.primitive.AzQueuedAnimation;
 import mod.azure.azurelib.core2.animation.primitive.AzRawAnimation;
@@ -52,13 +41,15 @@ public class AzAnimationController<T> {
 
     protected final Map<String, AzRawAnimation> triggerableAnimations = new Object2ObjectOpenHashMap<>(0);
 
+    private final AzAnimationQueue animationQueue;
+
     private final AzAnimator<T> animator;
 
     private final AzBoneSnapshotCache boneSnapshotCache;
 
     private final AzKeyFrameCallbackManager<T> keyFrameCallbackManager;
 
-    protected Queue<AzQueuedAnimation> animationQueue = new LinkedList<>();
+    private final AzKeyFrameProcessor<T> keyFrameProcessor;
 
     protected boolean isJustStarting = false;
 
@@ -101,9 +92,11 @@ public class AzAnimationController<T> {
         this.animator = animator;
         this.name = name;
         this.transitionLength = transitionTickTime;
+        this.animationQueue = new AzAnimationQueue();
         this.boneSnapshotCache = new AzBoneSnapshotCache();
         this.keyFrameCallbacks = AzKeyFrameCallbacks.noop();
         this.keyFrameCallbackManager = new AzKeyFrameCallbackManager<>(this);
+        this.keyFrameProcessor = new AzKeyFrameProcessor<>(this);
     }
 
     public AzAnimationController<T> setKeyFrameCallbacks(@NotNull AzKeyFrameCallbacks<T> keyFrameCallbacks) {
@@ -297,7 +290,8 @@ public class AzAnimationController<T> {
             var animations = animator.getAnimationProcessor().buildAnimationQueue(animatable, rawAnimation);
 
             if (animations != null) {
-                this.animationQueue = animations;
+                animationQueue.clear();
+                animationQueue.addAll(animations);
                 this.currentRawAnimation = rawAnimation;
                 this.shouldResetTick = true;
                 this.animationState = AzAnimationControllerState.TRANSITIONING;
@@ -415,11 +409,22 @@ public class AzAnimationController<T> {
         }
 
         if (getAnimationState() == AzAnimationControllerState.RUNNING) {
-            processCurrentAnimation(animatable, adjustedTick, seekTime, crashWhenCantFindBone);
+            keyFrameProcessor.runCurrentAnimation(
+                boneAnimationQueues,
+                animatable,
+                adjustedTick,
+                seekTime,
+                crashWhenCantFindBone
+            );
+            var canTransition = transitionLength == 0 && shouldResetTick;
+
+            if (canTransition && animationState == AzAnimationControllerState.TRANSITIONING) {
+                this.currentAnimation = this.animationQueue.next();
+            }
         } else if (animationState == AzAnimationControllerState.TRANSITIONING) {
             if (adjustedTick == 0 || isJustStarting) {
                 this.justStartedTransition = false;
-                this.currentAnimation = animationQueue.poll();
+                this.currentAnimation = animationQueue.next();
 
                 keyFrameCallbackManager.reset();
 
@@ -431,158 +436,13 @@ public class AzAnimationController<T> {
             }
 
             if (currentAnimation != null) {
-                MolangParser.INSTANCE.setValue(MolangQueries.ANIM_TIME, () -> 0);
-
-                for (var boneAnimation : currentAnimation.animation().boneAnimations()) {
-                    var boneAnimationQueue = boneAnimationQueues.get(boneAnimation.boneName());
-                    var boneSnapshot = boneSnapshotCache.getOrNull(boneAnimation.boneName());
-                    var bone = bones.get(boneAnimation.boneName());
-
-                    if (bone == null) {
-                        if (crashWhenCantFindBone)
-                            throw new NoSuchElementException("Could not find bone: " + boneAnimation.boneName());
-
-                        continue;
-                    }
-
-                    var rotationKeyFrames = boneAnimation.rotationKeyFrames();
-                    var positionKeyFrames = boneAnimation.positionKeyFrames();
-                    var scaleKeyFrames = boneAnimation.scaleKeyFrames();
-
-                    if (!rotationKeyFrames.xKeyframes().isEmpty()) {
-                        boneAnimationQueue.addNextRotation(
-                            null,
-                            adjustedTick,
-                            transitionLength,
-                            boneSnapshot,
-                            bone.getInitialSnapshot(),
-                            getAnimationPointAtTick(rotationKeyFrames.xKeyframes(), 0, true, Axis.X),
-                            getAnimationPointAtTick(rotationKeyFrames.yKeyframes(), 0, true, Axis.Y),
-                            getAnimationPointAtTick(rotationKeyFrames.zKeyframes(), 0, true, Axis.Z)
-                        );
-                    }
-
-                    if (!positionKeyFrames.xKeyframes().isEmpty()) {
-                        boneAnimationQueue.addNextPosition(
-                            null,
-                            adjustedTick,
-                            transitionLength,
-                            boneSnapshot,
-                            getAnimationPointAtTick(positionKeyFrames.xKeyframes(), 0, false, Axis.X),
-                            getAnimationPointAtTick(positionKeyFrames.yKeyframes(), 0, false, Axis.Y),
-                            getAnimationPointAtTick(positionKeyFrames.zKeyframes(), 0, false, Axis.Z)
-                        );
-                    }
-
-                    if (!scaleKeyFrames.xKeyframes().isEmpty()) {
-                        boneAnimationQueue.addNextScale(
-                            null,
-                            adjustedTick,
-                            transitionLength,
-                            boneSnapshot,
-                            getAnimationPointAtTick(scaleKeyFrames.xKeyframes(), 0, false, Axis.X),
-                            getAnimationPointAtTick(scaleKeyFrames.yKeyframes(), 0, false, Axis.Y),
-                            getAnimationPointAtTick(scaleKeyFrames.zKeyframes(), 0, false, Axis.Z)
-                        );
-                    }
-                }
-            }
-        }
-    }
-
-    /**
-     * Handle the current animation's state modifications and translations
-     *
-     * @param adjustedTick          The controller-adjusted tick for animation purposes
-     * @param seekTime              The lerped tick (current tick + partial tick)
-     * @param crashWhenCantFindBone Whether the controller should throw an exception when unable to find the required
-     *                              bone, or continue with the remaining bones
-     */
-    protected void processCurrentAnimation(
-        T animatable,
-        double adjustedTick,
-        double seekTime,
-        boolean crashWhenCantFindBone
-    ) {
-        if (adjustedTick >= currentAnimation.animation().length()) {
-            if (
-                currentAnimation.loopType().shouldPlayAgain(animatable, this, this.currentAnimation.animation())
-            ) {
-                if (animationState != AzAnimationControllerState.PAUSED) {
-                    this.shouldResetTick = true;
-
-                    adjustedTick = adjustTick(animatable, seekTime);
-                    keyFrameCallbackManager.reset();
-                }
-            } else {
-                var nextAnimation = animationQueue.peek();
-
-                keyFrameCallbackManager.reset();
-
-                if (nextAnimation == null) {
-                    this.animationState = AzAnimationControllerState.STOPPED;
-
-                    return;
-                } else {
-                    this.animationState = AzAnimationControllerState.TRANSITIONING;
-                    this.shouldResetTick = true;
-                    this.currentAnimation = nextAnimation;
-                }
-            }
-        }
-
-        final double finalAdjustedTick = adjustedTick;
-
-        MolangParser.INSTANCE.setMemoizedValue(MolangQueries.ANIM_TIME, () -> finalAdjustedTick / 20d);
-
-        for (var boneAnimation : currentAnimation.animation().boneAnimations()) {
-            var boneAnimationQueue = boneAnimationQueues.get(boneAnimation.boneName());
-
-            if (boneAnimationQueue == null) {
-                if (crashWhenCantFindBone)
-                    throw new NoSuchElementException("Could not find bone: " + boneAnimation.boneName());
-
-                continue;
-            }
-
-            var rotationKeyFrames = boneAnimation.rotationKeyFrames();
-            var positionKeyFrames = boneAnimation.positionKeyFrames();
-            var scaleKeyFrames = boneAnimation.scaleKeyFrames();
-
-            if (!rotationKeyFrames.xKeyframes().isEmpty()) {
-                boneAnimationQueue.addRotations(
-                    getAnimationPointAtTick(rotationKeyFrames.xKeyframes(), adjustedTick, true, Axis.X),
-                    getAnimationPointAtTick(rotationKeyFrames.yKeyframes(), adjustedTick, true, Axis.Y),
-                    getAnimationPointAtTick(rotationKeyFrames.zKeyframes(), adjustedTick, true, Axis.Z)
+                keyFrameProcessor.transitionFromCurrentAnimation(
+                    boneAnimationQueues,
+                    bones,
+                    crashWhenCantFindBone,
+                    adjustedTick
                 );
             }
-
-            if (!positionKeyFrames.xKeyframes().isEmpty()) {
-                boneAnimationQueue.addPositions(
-                    getAnimationPointAtTick(positionKeyFrames.xKeyframes(), adjustedTick, false, Axis.X),
-                    getAnimationPointAtTick(positionKeyFrames.yKeyframes(), adjustedTick, false, Axis.Y),
-                    getAnimationPointAtTick(positionKeyFrames.zKeyframes(), adjustedTick, false, Axis.Z)
-                );
-            }
-
-            if (!scaleKeyFrames.xKeyframes().isEmpty()) {
-                boneAnimationQueue.addScales(
-                    getAnimationPointAtTick(scaleKeyFrames.xKeyframes(), adjustedTick, false, Axis.X),
-                    getAnimationPointAtTick(scaleKeyFrames.yKeyframes(), adjustedTick, false, Axis.Y),
-                    getAnimationPointAtTick(scaleKeyFrames.zKeyframes(), adjustedTick, false, Axis.Z)
-                );
-            }
-        }
-
-        adjustedTick += this.transitionLength;
-
-        keyFrameCallbackManager.handle(animatable, adjustedTick);
-
-        if (
-            this.transitionLength == 0 && this.shouldResetTick
-                && this.animationState == AzAnimationControllerState.TRANSITIONING
-        ) {
-            this.currentAnimation = this.animationQueue.poll();
         }
     }
 
@@ -607,7 +467,7 @@ public class AzAnimationController<T> {
      * @return 0 if {@link AzAnimationController#shouldResetTick} is set to false, or a
      *         {@link AzAnimationController#animationSpeedModifier} modified value otherwise
      */
-    protected double adjustTick(T animatable, double tick) {
+    public double adjustTick(T animatable, double tick) {
         if (!shouldResetTick) {
             return animationSpeedModifier.applyAsDouble(animatable) * Math.max(tick - tickOffset, 0);
         }
@@ -622,65 +482,6 @@ public class AzAnimationController<T> {
     }
 
     /**
-     * Convert a {@link KeyframeLocation} to an {@link AnimationPoint}
-     */
-    protected AnimationPoint getAnimationPointAtTick(
-        List<Keyframe<IValue>> frames,
-        double tick,
-        boolean isRotation,
-        Axis axis
-    ) {
-        var location = getCurrentKeyFrameLocation(frames, tick);
-        var currentFrame = location.keyframe();
-        var startValue = currentFrame.startValue().get();
-        var endValue = currentFrame.endValue().get();
-
-        if (isRotation) {
-            if (!(currentFrame.startValue() instanceof Constant)) {
-                startValue = Math.toRadians(startValue);
-
-                if (axis == Axis.X || axis == Axis.Y) {
-                    startValue *= -1;
-                }
-            }
-
-            if (!(currentFrame.endValue() instanceof Constant)) {
-                endValue = Math.toRadians(endValue);
-
-                if (axis == Axis.X || axis == Axis.Y) {
-                    endValue *= -1;
-                }
-            }
-        }
-
-        return new AnimationPoint(currentFrame, location.startTick(), currentFrame.length(), startValue, endValue);
-    }
-
-    /**
-     * Returns the {@link Keyframe} relevant to the current tick time
-     *
-     * @param frames     The list of {@code KeyFrames} to filter through
-     * @param ageInTicks The current tick time
-     * @return A new {@code KeyFrameLocation} containing the current {@code KeyFrame} and the tick time used to find it
-     */
-    protected KeyframeLocation<Keyframe<IValue>> getCurrentKeyFrameLocation(
-        List<Keyframe<IValue>> frames,
-        double ageInTicks
-    ) {
-        var totalFrameTime = 0;
-
-        for (var frame : frames) {
-            totalFrameTime += frame.length();
-
-            if (totalFrameTime > ageInTicks) {
-                return new KeyframeLocation<>(frame, (ageInTicks - (totalFrameTime - frame.length())));
-            }
-        }
-
-        return new KeyframeLocation<>(frames.get(frames.size() - 1), ageInTicks);
-    }
-
-    /**
      * Returns the current state of this controller.
      */
     public AzAnimationControllerState getAnimationState() {
@@ -691,7 +492,35 @@ public class AzAnimationController<T> {
         this.animationState = animationState;
     }
 
+    public AzAnimationQueue getAnimationQueue() {
+        return animationQueue;
+    }
+
+    public AzBoneSnapshotCache getBoneSnapshotCache() {
+        return boneSnapshotCache;
+    }
+
     public AzKeyFrameCallbacks<T> getKeyFrameCallbacks() {
         return keyFrameCallbacks;
+    }
+
+    public AzKeyFrameCallbackManager<T> getKeyFrameCallbackManager() {
+        return keyFrameCallbackManager;
+    }
+
+    public double getTransitionLength() {
+        return transitionLength;
+    }
+
+    public boolean shouldResetTick() {
+        return shouldResetTick;
+    }
+
+    public void setShouldResetTick(boolean shouldResetTick) {
+        this.shouldResetTick = shouldResetTick;
+    }
+
+    public void setCurrentAnimation(AzQueuedAnimation currentAnimation) {
+        this.currentAnimation = currentAnimation;
     }
 }
---
+++ common/src/main/java/mod/azure/azurelib/core2/animation/controller/keyframe/AzKeyFrameProcessor.java
@@ -0,0 +1,251 @@
+package mod.azure.azurelib.core2.animation.controller.keyframe;
+
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+import mod.azure.azurelib.core.animatable.model.CoreGeoBone;
+import mod.azure.azurelib.core.keyframe.AnimationPoint;
+import mod.azure.azurelib.core.keyframe.BoneAnimationQueue;
+import mod.azure.azurelib.core.keyframe.Keyframe;
+import mod.azure.azurelib.core.keyframe.KeyframeLocation;
+import mod.azure.azurelib.core.math.Constant;
+import mod.azure.azurelib.core.math.IValue;
+import mod.azure.azurelib.core.molang.MolangParser;
+import mod.azure.azurelib.core.molang.MolangQueries;
+import mod.azure.azurelib.core.object.Axis;
+import mod.azure.azurelib.core2.animation.controller.AzAnimationController;
+import mod.azure.azurelib.core2.animation.controller.AzAnimationControllerState;
+
+public class AzKeyFrameProcessor<T> {
+
+    private final AzAnimationController<T> animationController;
+
+    public AzKeyFrameProcessor(AzAnimationController<T> animationController) {
+        this.animationController = animationController;
+    }
+
+    /**
+     * Handle the current animation's state modifications and translations
+     *
+     * @param boneAnimationQueues
+     * @param adjustedTick          The controller-adjusted tick for animation purposes
+     * @param seekTime              The lerped tick (current tick + partial tick)
+     * @param crashWhenCantFindBone Whether the controller should throw an exception when unable to find the required
+     *                              bone, or continue with the remaining bones
+     */
+    public void runCurrentAnimation(
+        Map<String, BoneAnimationQueue> boneAnimationQueues,
+        T animatable,
+        double adjustedTick,
+        double seekTime,
+        boolean crashWhenCantFindBone
+    ) {
+        var animationQueue = animationController.getAnimationQueue();
+        var animationState = animationController.getAnimationState();
+        var currentAnimation = animationController.getCurrentAnimation();
+        var keyFrameCallbackManager = animationController.getKeyFrameCallbackManager();
+        var transitionLength = animationController.getTransitionLength();
+
+        if (adjustedTick >= currentAnimation.animation().length()) {
+            if (
+                currentAnimation.loopType()
+                    .shouldPlayAgain(animatable, animationController, currentAnimation.animation())
+            ) {
+                if (animationState != AzAnimationControllerState.PAUSED) {
+                    animationController.setShouldResetTick(true);
+
+                    adjustedTick = animationController.adjustTick(animatable, seekTime);
+                    keyFrameCallbackManager.reset();
+                }
+            } else {
+                var nextAnimation = animationQueue.peek();
+
+                keyFrameCallbackManager.reset();
+
+                if (nextAnimation == null) {
+                    animationController.setAnimationState(AzAnimationControllerState.STOPPED);
+
+                    return;
+                } else {
+                    animationController.setAnimationState(AzAnimationControllerState.TRANSITIONING);
+                    animationController.setShouldResetTick(true);
+                    animationController.setCurrentAnimation(nextAnimation);
+                }
+            }
+        }
+
+        final double finalAdjustedTick = adjustedTick;
+
+        MolangParser.INSTANCE.setMemoizedValue(MolangQueries.ANIM_TIME, () -> finalAdjustedTick / 20d);
+
+        for (var boneAnimation : currentAnimation.animation().boneAnimations()) {
+            var boneAnimationQueue = boneAnimationQueues.get(boneAnimation.boneName());
+
+            if (boneAnimationQueue == null) {
+                if (crashWhenCantFindBone)
+                    throw new NoSuchElementException("Could not find bone: " + boneAnimation.boneName());
+
+                continue;
+            }
+
+            var rotationKeyFrames = boneAnimation.rotationKeyFrames();
+            var positionKeyFrames = boneAnimation.positionKeyFrames();
+            var scaleKeyFrames = boneAnimation.scaleKeyFrames();
+
+            if (!rotationKeyFrames.xKeyframes().isEmpty()) {
+                boneAnimationQueue.addRotations(
+                    getAnimationPointAtTick(rotationKeyFrames.xKeyframes(), adjustedTick, true, Axis.X),
+                    getAnimationPointAtTick(rotationKeyFrames.yKeyframes(), adjustedTick, true, Axis.Y),
+                    getAnimationPointAtTick(rotationKeyFrames.zKeyframes(), adjustedTick, true, Axis.Z)
+                );
+            }
+
+            if (!positionKeyFrames.xKeyframes().isEmpty()) {
+                boneAnimationQueue.addPositions(
+                    getAnimationPointAtTick(positionKeyFrames.xKeyframes(), adjustedTick, false, Axis.X),
+                    getAnimationPointAtTick(positionKeyFrames.yKeyframes(), adjustedTick, false, Axis.Y),
+                    getAnimationPointAtTick(positionKeyFrames.zKeyframes(), adjustedTick, false, Axis.Z)
+                );
+            }
+
+            if (!scaleKeyFrames.xKeyframes().isEmpty()) {
+                boneAnimationQueue.addScales(
+                    getAnimationPointAtTick(scaleKeyFrames.xKeyframes(), adjustedTick, false, Axis.X),
+                    getAnimationPointAtTick(scaleKeyFrames.yKeyframes(), adjustedTick, false, Axis.Y),
+                    getAnimationPointAtTick(scaleKeyFrames.zKeyframes(), adjustedTick, false, Axis.Z)
+                );
+            }
+        }
+
+        adjustedTick += transitionLength;
+
+        keyFrameCallbackManager.handle(animatable, adjustedTick);
+    }
+
+    public void transitionFromCurrentAnimation(
+        Map<String, BoneAnimationQueue> boneAnimationQueues,
+        Map<String, CoreGeoBone> bones,
+        boolean crashWhenCantFindBone,
+        double adjustedTick
+    ) {
+        var boneSnapshotCache = animationController.getBoneSnapshotCache();
+        var currentAnimation = animationController.getCurrentAnimation();
+        var transitionLength = animationController.getTransitionLength();
+
+        MolangParser.INSTANCE.setValue(MolangQueries.ANIM_TIME, () -> 0);
+
+        for (var boneAnimation : currentAnimation.animation().boneAnimations()) {
+            var boneAnimationQueue = boneAnimationQueues.get(boneAnimation.boneName());
+            var boneSnapshot = boneSnapshotCache.getOrNull(boneAnimation.boneName());
+            var bone = bones.get(boneAnimation.boneName());
+
+            if (bone == null) {
+                if (crashWhenCantFindBone)
+                    throw new NoSuchElementException("Could not find bone: " + boneAnimation.boneName());
+
+                continue;
+            }
+
+            var rotationKeyFrames = boneAnimation.rotationKeyFrames();
+            var positionKeyFrames = boneAnimation.positionKeyFrames();
+            var scaleKeyFrames = boneAnimation.scaleKeyFrames();
+
+            if (!rotationKeyFrames.xKeyframes().isEmpty()) {
+                boneAnimationQueue.addNextRotation(
+                    null,
+                    adjustedTick,
+                    transitionLength,
+                    boneSnapshot,
+                    bone.getInitialSnapshot(),
+                    getAnimationPointAtTick(rotationKeyFrames.xKeyframes(), 0, true, Axis.X),
+                    getAnimationPointAtTick(rotationKeyFrames.yKeyframes(), 0, true, Axis.Y),
+                    getAnimationPointAtTick(rotationKeyFrames.zKeyframes(), 0, true, Axis.Z)
+                );
+            }
+
+            if (!positionKeyFrames.xKeyframes().isEmpty()) {
+                boneAnimationQueue.addNextPosition(
+                    null,
+                    adjustedTick,
+                    transitionLength,
+                    boneSnapshot,
+                    getAnimationPointAtTick(positionKeyFrames.xKeyframes(), 0, false, Axis.X),
+                    getAnimationPointAtTick(positionKeyFrames.yKeyframes(), 0, false, Axis.Y),
+                    getAnimationPointAtTick(positionKeyFrames.zKeyframes(), 0, false, Axis.Z)
+                );
+            }
+
+            if (!scaleKeyFrames.xKeyframes().isEmpty()) {
+                boneAnimationQueue.addNextScale(
+                    null,
+                    adjustedTick,
+                    transitionLength,
+                    boneSnapshot,
+                    getAnimationPointAtTick(scaleKeyFrames.xKeyframes(), 0, false, Axis.X),
+                    getAnimationPointAtTick(scaleKeyFrames.yKeyframes(), 0, false, Axis.Y),
+                    getAnimationPointAtTick(scaleKeyFrames.zKeyframes(), 0, false, Axis.Z)
+                );
+            }
+        }
+    }
+
+    /**
+     * Convert a {@link KeyframeLocation} to an {@link AnimationPoint}
+     */
+    private AnimationPoint getAnimationPointAtTick(
+        List<Keyframe<IValue>> frames,
+        double tick,
+        boolean isRotation,
+        Axis axis
+    ) {
+        var location = getCurrentKeyFrameLocation(frames, tick);
+        var currentFrame = location.keyframe();
+        var startValue = currentFrame.startValue().get();
+        var endValue = currentFrame.endValue().get();
+
+        if (isRotation) {
+            if (!(currentFrame.startValue() instanceof Constant)) {
+                startValue = Math.toRadians(startValue);
+
+                if (axis == Axis.X || axis == Axis.Y) {
+                    startValue *= -1;
+                }
+            }
+
+            if (!(currentFrame.endValue() instanceof Constant)) {
+                endValue = Math.toRadians(endValue);
+
+                if (axis == Axis.X || axis == Axis.Y) {
+                    endValue *= -1;
+                }
+            }
+        }
+
+        return new AnimationPoint(currentFrame, location.startTick(), currentFrame.length(), startValue, endValue);
+    }
+
+    /**
+     * Returns the {@link Keyframe} relevant to the current tick time
+     *
+     * @param frames     The list of {@code KeyFrames} to filter through
+     * @param ageInTicks The current tick time
+     * @return A new {@code KeyFrameLocation} containing the current {@code KeyFrame} and the tick time used to find it
+     */
+    protected KeyframeLocation<Keyframe<IValue>> getCurrentKeyFrameLocation(
+        List<Keyframe<IValue>> frames,
+        double ageInTicks
+    ) {
+        var totalFrameTime = 0;
+
+        for (var frame : frames) {
+            totalFrameTime += frame.length();
+
+            if (totalFrameTime > ageInTicks) {
+                return new KeyframeLocation<>(frame, (ageInTicks - (totalFrameTime - frame.length())));
+            }
+        }
+
+        return new KeyframeLocation<>(frames.get(frames.size() - 1), ageInTicks);
+    }
+}
