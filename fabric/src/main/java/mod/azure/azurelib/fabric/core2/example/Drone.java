c4cbe3edd2c9bbf1c90675a94c86505cc0640437
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-12-14 21:15:45 -0500

    Add drone walking example

--- fabric/src/main/java/mod/azure/azurelib/fabric/core2/example/Drone.java
+++ fabric/src/main/java/mod/azure/azurelib/fabric/core2/example/Drone.java
@@ -1,7 +1,11 @@
 package mod.azure.azurelib.fabric.core2.example;
 
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
+import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
+import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomStrollGoal;
 import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 
 import mod.azure.azurelib.core2.animation.AzAnimationDispatcher;
@@ -9,27 +13,31 @@ import mod.azure.azurelib.core2.animation.AzAnimationDispatcher;
 public class Drone extends Monster {
 
     private final AzAnimationDispatcher<Drone> animationDispatcher;
+    private final MoveAnalysis moveAnalysis;
 
     public Drone(EntityType<? extends Monster> entityType, Level level) {
         super(entityType, level);
         this.animationDispatcher = new AzAnimationDispatcher<>(this);
+        this.moveAnalysis = new MoveAnalysis(this);
     }
-
-    private boolean walk;
 
     public void tick() {
         super.tick();
+        moveAnalysis.update();
 
         if (this.level().isClientSide) {
-            if (this.tickCount % 40 == 0) {
-                walk = !walk;
-            }
-
-            var animName = walk
-                ? "animation.walk"
-                : "animation.idle";
-
+            var isMovingOnGround = moveAnalysis.isMovingHorizontally() && onGround();
+            var animName = isMovingOnGround
+                    ? "animation.walk"
+                    : "animation.idle";
             animationDispatcher.dispatchFromClient("base_controller", animName);
+        } else {
+            // Doing other stuff server-side...
         }
+    }
+
+    @Override
+    protected void registerGoals() {
+        this.goalSelector.addGoal(7, new WaterAvoidingRandomStrollGoal(this, 0.25F));
     }
 }
---
+++ fabric/src/main/java/mod/azure/azurelib/fabric/core2/example/MoveAnalysis.java
@@ -0,0 +1,57 @@
+package mod.azure.azurelib.fabric.core2.example;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.phys.Vec3;
+
+public class MoveAnalysis {
+
+    private final Entity entity;
+
+    private int lastTick;
+    private Vec3 lastPosition;
+
+    private double deltaX;
+    private double deltaY;
+    private double deltaZ;
+
+    public MoveAnalysis(Entity entity) {
+        this.entity = entity;
+        this.lastPosition = entity.position();
+    }
+
+    public void update() {
+        if (entity.tickCount == lastTick) {
+            // Only update on tick differences.
+            return;
+        }
+
+        var prevPos = lastPosition;
+        var prevPosX = prevPos.x;
+        var prevPosY = prevPos.y;
+        var prevPosZ = prevPos.z;
+
+        var pos = entity.position();
+        var posX = pos.x;
+        var posY = pos.y;
+        var posZ = pos.z;
+
+        this.deltaX = posX - prevPosX;
+        this.deltaY = posY - prevPosY;
+        this.deltaZ = posZ - prevPosZ;
+
+        this.lastPosition = entity.position();
+        this.lastTick = entity.tickCount;
+    }
+
+    public boolean isMovingHorizontally() {
+        return deltaX != 0 || deltaZ != 0;
+    }
+
+    public boolean isMovingVertically() {
+        return deltaY != 0;
+    }
+
+    public boolean isMoving() {
+        return isMovingHorizontally() || isMovingVertically();
+    }
+}
