3b4858829042cd8327ede407ee6cb7f35f07d132
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-12-21 17:40:57 -0500

    Hey lookie, the mixin works now that's it's registered and using the right method

--- common/src/main/java/mod/azure/azurelib/common/internal/mixins/ItemStackMixin_AzItemStackIdentityRegistry.java
+++ common/src/main/java/mod/azure/azurelib/common/internal/mixins/ItemStackMixin_AzItemStackIdentityRegistry.java
@@ -2,9 +2,11 @@ package mod.azure.azurelib.common.internal.mixins;
 
 import net.minecraft.core.component.PatchedDataComponentMap;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.ItemLike;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.UUID;
 
@@ -15,13 +17,13 @@ import mod.azure.azurelib.core2.animation.cache.AzIdentityRegistry;
 @Mixin(ItemStack.class)
 public class ItemStackMixin_AzItemStackIdentityRegistry {
 
-    @Inject(method = "<init>", at = @At("TAIL"))
-    public void addIdentity() {
+    @Inject(
+        method = "<init>(Lnet/minecraft/world/level/ItemLike;ILnet/minecraft/core/component/PatchedDataComponentMap;)V",
+        at = @At("TAIL")
+    )
+    public void az_addIdentityComponent(ItemLike item, int count, PatchedDataComponentMap components, CallbackInfo ci) {
         var self = AzureLibUtil.<ItemStack>self(this);
-        if (
-            AzIdentityRegistry.hasIdentity(self.getItem()) && self
-                .getComponents() instanceof PatchedDataComponentMap components && !components.has(AzureLib.AZ_ID.get())
-        ) {
+        if (AzIdentityRegistry.hasIdentity(self.getItem()) && !components.has(AzureLib.AZ_ID.get())) {
             components.set(AzureLib.AZ_ID.get(), UUID.randomUUID());
         }
     }
