9223071eb01136635945c97c2d64bacd5943fa1b
Boston Vanseghi <bvanseg@gmail.com>
2024-12-21 22:33:11 -0500

    Play equip anim for the doom armor pieces only once.
    Signed-off-by: = <bvanseg@gmail.com>

--- fabric/src/main/java/mod/azure/azurelib/fabric/core2/example/armors/AzDoomArmorAnimator.java
+++ fabric/src/main/java/mod/azure/azurelib/fabric/core2/example/armors/AzDoomArmorAnimator.java
@@ -8,6 +8,7 @@ import mod.azure.azurelib.common.internal.common.AzureLib;
 import mod.azure.azurelib.core2.animation.controller.AzAnimationController;
 import mod.azure.azurelib.core2.animation.controller.AzAnimationControllerContainer;
 import mod.azure.azurelib.core2.animation.impl.AzItemAnimator;
+import mod.azure.azurelib.core2.animation.primitive.AzLoopType;
 import mod.azure.azurelib.core2.animation.primitive.AzRawAnimation;
 
 public class AzDoomArmorAnimator extends AzItemAnimator {
@@ -18,7 +19,8 @@ public class AzDoomArmorAnimator extends AzItemAnimator {
 
     private static final String EQUIP_ANIMATION_NAME = "equipping";
 
-    private static final AzRawAnimation EQUIP_ANIMATION = AzRawAnimation.begin().thenLoop(EQUIP_ANIMATION_NAME);
+    private static final AzRawAnimation EQUIP_ANIMATION = AzRawAnimation.begin()
+        .then(EQUIP_ANIMATION_NAME, AzLoopType.PLAY_ONCE);
 
     @Override
     public void registerControllers(AzAnimationControllerContainer<ItemStack> animationControllerContainer) {
