9ce3c00336e9b81073160c79470e41fc990f6bdb
Boston Vanseghi <bvanseg@gmail.com>
2024-12-23 17:05:33 -0500

    Removed AzCommandBuilder's type parameter (not necessary).
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/mod/azure/azurelib/core2/animation/dispatch/command/AzCommandBuilder.java
+++ common/src/main/java/mod/azure/azurelib/core2/animation/dispatch/command/AzCommandBuilder.java
@@ -5,17 +5,12 @@ import java.util.List;
 
 import mod.azure.azurelib.core2.animation.dispatch.command.action.AzAction;
 
-public abstract class AzCommandBuilder<T extends AzCommandBuilder<T>> {
+public abstract class AzCommandBuilder {
 
     protected final List<AzAction> actions;
 
     protected AzCommandBuilder() {
         this.actions = new ArrayList<>();
-    }
-
-    @SuppressWarnings("unchecked")
-    protected T self() {
-        return (T) this;
     }
 
     public AzCommand build() {
--- common/src/main/java/mod/azure/azurelib/core2/animation/dispatch/command/AzRootCommandBuilder.java
+++ common/src/main/java/mod/azure/azurelib/core2/animation/dispatch/command/AzRootCommandBuilder.java
@@ -11,7 +11,7 @@ import mod.azure.azurelib.core2.animation.dispatch.command.action.impl.root.AzRo
 import mod.azure.azurelib.core2.animation.dispatch.command.sequence.AzAnimationSequenceBuilder;
 import mod.azure.azurelib.core2.animation.easing.AzEasingType;
 
-public class AzRootCommandBuilder extends AzCommandBuilder<AzRootCommandBuilder> {
+public class AzRootCommandBuilder extends AzCommandBuilder {
 
     public AzRootCommandBuilder append(AzCommand command) {
         actions.addAll(command.actions());
@@ -20,27 +20,27 @@ public class AzRootCommandBuilder extends AzCommandBuilder<AzRootCommandBuilder>
 
     public AzRootCommandBuilder cancelAll() {
         actions.add(new AzRootCancelAllAction());
-        return self();
+        return this;
     }
 
     public AzRootCommandBuilder setEasingType(AzEasingType easingType) {
         actions.add(new AzRootSetEasingTypeAction(easingType));
-        return self();
+        return this;
     }
 
     public AzRootCommandBuilder setSpeed(float speed) {
         actions.add(new AzRootSetAnimationSpeedAction(speed));
-        return self();
+        return this;
     }
 
     public AzRootCommandBuilder setTransitionSpeed(float transitionSpeed) {
         actions.add(new AzRootSetTransitionSpeedAction(transitionSpeed));
-        return self();
+        return this;
     }
 
     public AzRootCommandBuilder cancel(String controllerName) {
         actions.add(new AzRootCancelAction(controllerName));
-        return self();
+        return this;
     }
 
     public AzRootCommandBuilder play(String controllerName, String animationName) {
@@ -53,6 +53,6 @@ public class AzRootCommandBuilder extends AzCommandBuilder<AzRootCommandBuilder>
     ) {
         var sequence = builderUnaryOperator.apply(new AzAnimationSequenceBuilder()).build();
         actions.add(new AzRootPlayAnimationSequenceAction(controllerName, sequence));
-        return self();
+        return this;
     }
 }
