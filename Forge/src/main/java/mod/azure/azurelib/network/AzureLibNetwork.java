341d78ac1c8cf43a1724b6f060959b6806df0db7
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-12-24 11:43:03 -0500

    Remove unneeded Geo related methods/Split for Client/Server packet registration.

--- Forge/src/main/java/mod/azure/azurelib/network/AzureLibNetwork.java
+++ Forge/src/main/java/mod/azure/azurelib/network/AzureLibNetwork.java
@@ -13,9 +13,6 @@ import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import net.minecraftforge.fml.relauncher.Side;
 
-import javax.annotation.Nullable;
-import java.util.Map;
-
 /**
  * Network handling class for AzureLib.<br>
  * Handles packet registration and some networking functions
@@ -24,39 +21,18 @@ public final class AzureLibNetwork {
 	private static final String VER = "1";
 	private static final SimpleNetworkWrapper PACKET_CHANNEL = new SimpleNetworkWrapper(AzureLib.MOD_ID);
 
-	private static final Map<String, GeoAnimatable> SYNCED_ANIMATABLES = new Object2ObjectOpenHashMap<>();
-
-	public static void init() {
+	public static void initClient() {
 		int id = 0;
-		PACKET_CHANNEL.registerMessage(AzBlockEntityDispatchCommandPacket.class, AzBlockEntityDispatchCommandPacket.class, id++, Side.);
-		PACKET_CHANNEL.registerMessage(AzEntityDispatchCommandPacket.class, AzEntityDispatchCommandPacket.class, id++, Side.);
-		PACKET_CHANNEL.registerMessage(AzItemStackDispatchCommandPacket.class, AzItemStackDispatchCommandPacket.class, id++, Side.);
+		PACKET_CHANNEL.registerMessage(AzBlockEntityDispatchCommandPacket.class, AzBlockEntityDispatchCommandPacket.class, id++, Side.CLIENT);
+		PACKET_CHANNEL.registerMessage(AzEntityDispatchCommandPacket.class, AzEntityDispatchCommandPacket.class, id++, Side.CLIENT);
+		PACKET_CHANNEL.registerMessage(AzItemStackDispatchCommandPacket.class, AzItemStackDispatchCommandPacket.class, id++, Side.CLIENT);
 	}
 
-	/**
-	 * Registers a synced {@link GeoAnimatable} object for networking support.<br>
-	 * It is recommended that you don't call this directly, instead implementing and calling {@link mod.azure.azurelib.animatable.SingletonGeoAnimatable#registerSyncedAnimatable}
-	 */
-	synchronized public static void registerSyncedAnimatable(GeoAnimatable animatable) {
-		GeoAnimatable existing = SYNCED_ANIMATABLES.put(animatable.getClass().toString(), animatable);
-
-		if (existing == null)
-			AzureLib.LOGGER.debug("Registered SyncedAnimatable for " + animatable.getClass().toString());
-	}
-
-	/**
-	 * Gets a registered synced {@link GeoAnimatable} object by name
-	 * 
-	 * @param className
-	 */
-	@Nullable
-	public static GeoAnimatable getSyncedAnimatable(String className) {
-		GeoAnimatable animatable = SYNCED_ANIMATABLES.get(className);
-
-		if (animatable == null)
-			AzureLib.LOGGER.error("Attempting to retrieve unregistered synced animatable! (" + className + ")");
-
-		return animatable;
+	public static void initServer() {
+		int id = 0;
+		PACKET_CHANNEL.registerMessage(AzBlockEntityDispatchCommandPacket.class, AzBlockEntityDispatchCommandPacket.class, id++, Side.SERVER);
+		PACKET_CHANNEL.registerMessage(AzEntityDispatchCommandPacket.class, AzEntityDispatchCommandPacket.class, id++, Side.SERVER);
+		PACKET_CHANNEL.registerMessage(AzItemStackDispatchCommandPacket.class, AzItemStackDispatchCommandPacket.class, id++, Side.SERVER);
 	}
 
 	/**
