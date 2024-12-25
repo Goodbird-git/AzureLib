e4841feaf3d6a84a15ac4126966a04d3c1f6673b
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-12-25 10:32:54 -0500

    Update AzureLib.java

--- Forge/src/main/java/mod/azure/azurelib/AzureLib.java
+++ Forge/src/main/java/mod/azure/azurelib/AzureLib.java
@@ -19,7 +19,7 @@ public class AzureLib {
 	public static Logger LOGGER = LogManager.getLogger();
 	public static final String MOD_ID = "azurelib";
 	public static final String NAME = "AzureLib";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "3.0.0";
 	public static volatile boolean hasInitialized;
 
 	public static synchronized void initialize() {
