ee793e975aa3d1bd2aa9be076482f2a999cecff6
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-12-26 22:42:31 -0500

    Implement just for testing, still need to figure out why the mixin isn't applying correctly.

--- Forge/src/main/java/mod/azure/azurelib/examples/DoomHunter.java
+++ Forge/src/main/java/mod/azure/azurelib/examples/DoomHunter.java
@@ -1,9 +1,26 @@
 package mod.azure.azurelib.examples;
 
+import mod.azure.azurelib.animation.AzAnimator;
+import mod.azure.azurelib.animation.AzAnimatorAccessor;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Unique;
 
-public class DoomHunter extends EntityMob {
+public class DoomHunter extends EntityMob implements AzAnimatorAccessor<Entity> {
+
+    @Unique
+    private AzAnimator<Entity> animator;
+
+    @Override
+    public void setAnimator(AzAnimator<Entity> animator) {
+        this.animator = animator;
+    }
+
+    @Override
+    public AzAnimator<Entity> getAnimatorOrNull() {
+        return animator;
+    }
 
     private final DoomHunterAnimationDispatcher animationDispatcher;
 
